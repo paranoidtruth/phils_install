92e85dbdbc773ba52ac1779fa8ea782ecd8e2820
Josh <paranoidtruth@yahoo.com>
2018-01-14 20:31:20 -0500

    add usr text

--- add_user.sh
+++ add_user.sh
@@ -5,7 +5,7 @@ echo -n "=================================================================="
 if [ `whoami` = 'root' ]
   then
     echo -n "First let us create a New User."
-    read -p "Enter New Username (keep it simple): " uname 
+    read -p "Enter New Username (keep it simple and save it): " uname 
     adduser "$uname"
 	usermod -aG sudo "$uname"
 	su - "$uname" 
